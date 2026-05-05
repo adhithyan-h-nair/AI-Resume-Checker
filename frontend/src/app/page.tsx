a627d2fcc6287aef8d501923ec7e2e3dc1b0cdcb
Mr.H <adhithyanhnair99@gmail.com>
2026-05-05 10:50:50 +0000

    file input validation updated

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,20 +1,30 @@
 "use client" ;
 import { useState } from "react"
 export default function Home(){
+    const [file, setFile] = useState<File | null>(null)
     const [result, setResult] = useState("No analysis yet")
 
+    const handleFileChange = (e: any) => {
+        setFile(e.target.files[0])
+    }
+
     const handleAnalyze = () => {
-        setResult("Analyzing....Fake one for now")
+        if (!file) {
+            setResult("Please Upload File first")
+            return;
+        }
+
+        setResult(`Analyzing ${file.name} (fake for now)`)
     }
 
     return(
         <main style={{padding: "2rem"}}>
             <h1>AI Resume Analyzer</h1>
 
-            <input type="file" />
+            <input type="file" onChange={handleFileChange}/>
 
             <br /> <br />
-            <button onClick={handleAnalyze}>Analyze</button>
+            <button onClick={handleAnalyze} disabled={!file} >Analyze</button>
 
             <div style={{marginTop: "2rem"}}>
                 <h2>Result:</h2>
